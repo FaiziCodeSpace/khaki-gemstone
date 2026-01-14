38535775c8141298cfbe693c27d0397d2b9ceb73
FaiziCodeSpace <faizanwebdev1@gmail.com>
2026-01-14 20:45:08 +0500

    feat(api): implement advanced product filtering with MongoDB  logic

--- src/pages/public/ProductDetail.jsx
+++ src/pages/public/ProductDetail.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Footer } from "../../components/layout/Footer";
 import { ProductDetails } from "../../components/public/ProductDetail/ProductDetails";
 import { ProductImages } from "../../components/public/ProductDetail/ProductImages";
@@ -6,7 +7,9 @@ export function ProductDetailPage() {
   // Define layout constants to avoid long strings in the JSX
   const containerStyles = "px-6 md:px-8 lg:px-14 lgx:px-22 pt-36 md:pt-42";
   const flexStyles = "flex flex-col lg:flex-row gap-8 lg:gap-12 lgx:gap-15";
-
+  useEffect(()=>{
+    
+  },[])
   return (
     <div className="font-satoshi min-h-screen flex flex-col">
       <main className={`${containerStyles} ${flexStyles} flex-grow`}>
