420de0626f768c588f9a4d2c88febe735bb3f15c
FaiziCodeSpace <faizanwebdev1@gmail.com>
2026-01-30 22:14:51 +0500

    Feat: Transaction Integration done

---
+++ src/services/adminServices/transactions.js
@@ -0,0 +1,28 @@
+import api from "../api";
+
+export const getTransactions = async (params) => {
+  try {
+    const response = await api.get("/transactions", { params });
+    console.log(response.data);
+    return response.data; 
+  } catch (error) {
+    throw error.response?.data?.message || "Error fetching transactions";
+  }
+};
+
+export const getTransactionDetail = async (id) => {
+  try {
+    const response = await api.get(`/transactions/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || "Transaction not found";
+  }
+};
+
+
+export const formatEnum = (slug) => {
+  if (!slug) return "";
+  return slug.split("_").map(word => 
+    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  ).join(" ");
+};
